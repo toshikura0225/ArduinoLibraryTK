d1db7de6850c51b8074f81e312f38a63e2d921da
kuramata <t_kuramata@yahoo.co.jp>
2015-02-06 19:15:52 +0900

    first commit

---
+++ Pin.h
@@ -0,0 +1,11 @@
+﻿class Pin
+{
+	public:
+		Pin(int pinNumber);
+		int PinNumber;
+		int IsShifted();
+		int IsShifted(int keepingTimer);
+		
+	protected:
+		
+};
---
+++ Pin.ino
@@ -0,0 +1,19 @@
+﻿#include "Pin.h"
+
+#define KEEPING_TIMER 100
+
+Pin::Pin(int pinNumber)
+{
+	PinNumber = pinNumber;
+}
+
+
+int Pin::IsShifted()
+{
+	return 1;
+}
+
+int Pin::IsShifted(int keepingTimer)
+{
+	return 1;
+}
