7e7b1d488b9c05ef8c48cc9fce7d215eceb4c36e
kuramata <t_kuramata@yahoo.co.jp>
2015-03-23 20:05:00 +0900

    remove static

--- DigitalInputPin.ino
+++ DigitalInputPin.ino
@@ -1,49 +1,48 @@
 #include "DigitalInputPin.h"
 
-#define KEEPING_TIMER 10
-
 DigitalInputPin::DigitalInputPin(int argNumber)
 {
-	(*this).pinNumber = argNumber;
-	(*this).keepingTimer = 10;
-        pinMode(pinNumber, INPUT_PULLUP);
+	(*this).init(10);
 }
 
-DigitalInputPin::DigitalInputPin(int argNumber, int argTimer)
+DigitalInputPin::DigitalInputPin(int argNumber, int argKeepingConstant)
+{
+	(*this).init(argKeepingConstant);
+}
+
+DigitalInputPin::init(int argKeepingConstant)
 {
 	(*this).pinNumber = argNumber;
-	(*this).keepingTimer = argTimer;
-        pinMode(pinNumber, INPUT_PULLUP);
+	(*this).keepingConstant = 10;
+	pinMode(pinNumber, INPUT_PULLUP);
+	(*this).keepingCount = 0;
+	(*this).keepingState = digitalRead((*this).pinNumber);
+	(*this).preState = (*this).keepingState;		// 前回の状態
 }
-
 
 int DigitalInputPin::GetState()
 {
-  // digitalRead((*this).pinNumber);
-  static int keepingCount = 0;		// 状態が連続して安定した回数を
-  static int preState = digitalRead((*this).pinNumber);		// 前回の状態
-  static int keepingState = preState;
   int currentState = digitalRead((*this).pinNumber);	// 今回測定分の状態
   
   // 状態が連続して安定した回数をインクリメント
-  if (preState == currentState)
+  if ((*this).preState == currentState)
   {
-	  keepingCount++;
+      (*this).keepingCount++;
 
       // 閾値を超えたら状態変更とする
-      if (keepingCount >=(*this).keepingTimer)
+      if ((*this).keepingCount >=(*this).keepingConstant)
       {
-              keepingState = currentState;
-    	  keepingCount = (*this).keepingTimer;
+			(*this).keepingState = currentState;
+			(*this).keepingCount = (*this).keepingConstant;
       }
   }
   else
   {
-	  keepingCount = 0;
+	  (*this).keepingCount = 0;
   }
 
   
-  preState = currentState;
+  (*this).preState = currentState;
   
   return keepingState;
 }
@@ -52,13 +51,12 @@ int DigitalInputPin::GetState()
 // 変更がない場合は-1を返す
 int DigitalInputPin::IsShifted()
 {
-	static int preState = digitalRead((*this).pinNumber);
 	int currentState = (*this).GetState();
 	int retValue = -1;
-	if (preState != currentState)
+	if ((*this).preState != currentState)
 	{
 		retValue = currentState;
 	}
-	preState = currentState;
+	(*this).preState = currentState;
 	return retValue;
 }
